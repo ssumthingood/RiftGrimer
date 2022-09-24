149d0b78fc8852f8d6f6bfe017fbecb541610c80
ssumthingood <ssumthingood@gmail.com>
2022-09-25 02:38:28 +0900

    0925 riftgrimer - axios setting & need clear CORS issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { userApi } from "./lolapi";
 
 const Home: NextPage = () => {
+    console.log(process.env.NEXT_PUBLIC_RIOT_KEY);
+    console.log(userApi.rotation());
     return <></>;
 };
 
