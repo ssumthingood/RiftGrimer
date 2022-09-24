a96161a865d5dbe4c263d9e7d7c67ea141d2b1ce
ssumthingood <ssumthingood@gmail.com>
2022-09-24 13:04:47 +0900

    0924 tailwind setting

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./pages/**/*.{tsx,ts,js,jsx}", "./components/**/*.{tsx,ts,js,jsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
