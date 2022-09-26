5ef2215e40442d4e89e5291b789321c74557bf4a
ssumthingood <ssumthingood@gmail.com>
2022-09-26 11:11:39 +0900

    0926:API WORKING FINE / NEED TO MODULIZE

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,15 @@ const Home: NextPage = () => {
     };
 
     //이렇게 하면 cors가 안뜨는데... 일단 이런 식으로 따로 모듈화해두는게 좋을듯
-    axios
-        .get(`${process.env.NEXT_PUBLIC_RIOT_API}summoner/v4/summoners/by-name/%EB%B2%84%ED%8A%BC%EC%9D%84%EB%88%8C%EB%9F%AC%EC%A7%80%EA%B8%88%EB%94%B1?api_key=${process.env.NEXT_PUBLIC_RIOT_KEY}`)
-        .then((res) => {
-            console.log(res.data);
-        });
+    // axios
+    //     .get(`${process.env.NEXT_PUBLIC_RIOT_API}summoner/v4/summoners/by-name/%EB%B2%84%ED%8A%BC%EC%9D%84%EB%88%8C%EB%9F%AC%EC%A7%80%EA%B8%88%EB%94%B1?api_key=${process.env.NEXT_PUBLIC_RIOT_KEY}`)
+    //     .then((res) => {
+    //         console.log(res.data);
+    //     });
 
+    // axios.get(`${process.env.NEXT_PUBLIC_RIOT_API}platform/v3/champion-rotations?api_key=${process.env.NEXT_PUBLIC_RIOT_KEY}`).then((res) => {
+    //     console.log(res.data.freeChampionIds);
+    // });
     //이렇게 보내면 cors가 뜬다
     // axios
     //     .get("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EB%B2%84%ED%8A%BC%EC%9D%84%EB%88%8C%EB%9F%AC%EC%A7%80%EA%B8%88%EB%94%B1", {
@@ -33,6 +36,15 @@ const Home: NextPage = () => {
     //     .then((res) => {
     //         console.log(res);
     //     });
+
+    //모듈화 해야됨 - api 설계 자체는 성공
+    LOLapi.rotation().then((res) => {
+        console.log(res.data);
+    });
+
+    LOLapi.searchByName().then((res) => {
+        console.log(res.data);
+    });
 
     fetchPhotos();
     fetchPhotos2();
