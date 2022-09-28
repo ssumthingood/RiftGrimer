b0b9798bd3468d236f5def6024d5f5030205be4f
ssumthingood <ssumthingood@gmail.com>
2022-09-28 23:24:09 +0900

    success extract champion status my champion name

--- libs/client/utils.ts
+++ libs/client/utils.ts
@@ -1,9 +1,11 @@
+import { isBooleanObject } from "util/types";
+
 export function cls(...classnames: string[]) {
     return classnames.join(" ");
 }
 
-// export function getChampStatByName(name: string, stat: any): string {
-//     const ${name} = stat;
-//     console.log(thing);
-//     return thing.id;
-// }
+export function getChampStatByName(name: string, status: any): any {
+    const filtered = status[name];
+    //함수에 param 넣어서 그 param으로 JSON내의 필드를 찾고 싶을 때는 json[찾으려는 필드] 이런 식으로 찾으면 된다
+    return filtered;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { LOLapi, DDragonapi } from "@libs/lolapi";
 import { useState } from "react";
 import { useEffect } from "react";
-import { cls } from "@libs/client/utils";
+import { cls, getChampStatByName } from "@libs/client/utils";
 import Layout from "@components/Layout";
 import Link from "next/link";
 
@@ -21,6 +21,8 @@ const Home: NextPage = () => {
     };
 
     useEffect(() => {
+        const champname = "Aatrox";
+
         LOLapi.serverStatus().then((res) => {
             setServer(res.data.incidents);
         });
@@ -33,9 +35,11 @@ const Home: NextPage = () => {
             setChamps(res.data.data);
         });
 
-        DDragonapi.championStatusByName("Aatrox").then((res) => {
-            const { Aatrox } = res.data.data;
-            console.log(Aatrox.id);
+        DDragonapi.championStatusByName(champname).then((res) => {
+            // const { Aatrox } = res.data.data;
+            // console.log(Aatrox.name);
+
+            console.log(getChampStatByName(champname, res.data.data));
         });
     }, []);
 
